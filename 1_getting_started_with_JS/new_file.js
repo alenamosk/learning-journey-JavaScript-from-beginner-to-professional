5cf4ec864733b941aa51fc00ad964951684cdf27
alenamosk <almoskaeva@gmail.com>
2024-05-28 17:50:05 +0200

    Chapter project #1 is done

---
+++ 1_getting_started_with_JS/new_file.js
@@ -0,0 +1,4 @@
+// 1. In the JavaScript file, output your name into the console and add a multiple-line comment to your code.
+// 2. Try commenting out the console message in your JavaScript file so that nothing shows in the console.
+
+// console.log("Alena");

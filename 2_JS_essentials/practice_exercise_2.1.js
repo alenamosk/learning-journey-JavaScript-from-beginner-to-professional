db444c6d64cc024e1489af7cbbb1c65aaddb081f
alenamosk <almoskaeva@gmail.com>
2024-05-28 18:05:13 +0200

    Chapter 2: Practice exercise 2.1 - done

---
+++ 2_JS_essentials/practice_exercise_2.1.js
@@ -0,0 +1,15 @@
+// What are the types of these variables listed below? Verify this with typeof and output the result to the console:
+let str1 = "Laurence"; //string
+console.log(typeof str1);
+
+let str2 = "Svekis"; // string
+console.log(typeof str2);
+
+let val1 = undefined; // undefined
+console.log(typeof val1);
+
+let val2 = null; // object
+console.log(typeof val2);
+
+let myNum = 1000; //number
+console.log(typeof myNum);

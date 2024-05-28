bd9198b2af1da97aedc1f39eb569c2d9cadbb150
alenamosk <almoskaeva@gmail.com>
2024-05-28 18:09:58 +0200

    Chapter 2: Practice exercise 2.2 - done

---
+++ 2_JS_essentials/practice_exercise_2.2.js
@@ -0,0 +1,11 @@
+// Create a variable for your name, another one for your age, and another one for whether you can code JavaScript or not.
+
+// Log to the console the following sentence, where name, age and true/false are variables: Hello, my name is Maaike, I am 29 years old and I can code JavaScript: true.
+
+let myName = "Alena";
+let myAge = 32;
+let ableToCodeJS = true;
+
+console.log(
+  `Hello, my name is ${myName}, I am ${myAge} years old and I can code JavaScript: ${ableToCodeJS}.`
+);

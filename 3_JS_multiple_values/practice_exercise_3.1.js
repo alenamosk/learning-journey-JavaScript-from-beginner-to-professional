86054b26488b506531e4517a6ca7d43144387741
alenamosk <almoskaeva@gmail.com>
2024-05-29 14:18:59 +0200

    Chapter 3: Practice exercise 3.1 - done

---
+++ 3_JS_multiple_values/practice_exercise_3.1.js
@@ -0,0 +1,13 @@
+// Exercise from the book "JavaScript from Beginner to Professional"
+// Authors: Laurence Lars Svekis, Maaike van Putten, Codestars By Rob Percival
+// Link to the book: https://learning.oreilly.com/library/view/javascript-from-beginner/9781800562523/
+
+// Create an array to use as your shopping list with 3 items: "Milk," "Bread," and "Apples."
+// Check your list length in the console.
+// Update "Bread" to "Bananas."
+// Output your entire list to the console.
+
+shoppigList = ["Milk", "Bread", "Apples"];
+console.log(shoppigList.length);
+shoppigList[1] = "Bananas";
+console.log(shoppigList);

bed106388678c09c9da501969fbb7582f5f5a015
alenamosk <almoskaeva@gmail.com>
2024-06-06 14:30:27 +0200

    Chapter 4: Practice exercise 4.3 - done

---
+++ 4_logic-_statements/practice_exercise_4.3.js
@@ -0,0 +1,14 @@
+// Exercise from the book "JavaScript from Beginner to Professional"
+// Authors: Laurence Lars Svekis, Maaike van Putten, Codestars By Rob Percival
+// Link to the book: https://learning.oreilly.com/library/view/javascript-from-beginner/9781800562523/
+
+// Practice exercise 4.3
+
+// Create a Boolean value for an ID variable
+let ID = true;
+
+// Using a ternary operator, create a message variable that will check whether their ID is valid and either allow a person into a venue or not
+let access = ID ? "Please, come in" : "Sorry, I can't let you in";
+
+// Output the response to the console
+console.log(access);
